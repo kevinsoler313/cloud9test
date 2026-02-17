cfab9fcd73e30206f6789b8160cc0ab0556c7d25
Ubuntu <ubuntu@ip-172-31-90-56.ec2.internal>
2026-02-17 15:18:35 +0000

    first commit

---
+++ main2.py
@@ -0,0 +1,15 @@
+def funcion (numero):
+    if (numero <= 1):
+        print ("NO ES PRIMO")
+        return False
+    elif(numero == 2):
+        print ("NUMERO PRIMO")
+        return True
+    else:
+        for i in (2, numero-1, 1):
+            if (numero % 2 == 0):
+                print ("NO ES PRIMO")
+                return False
+            else:
+                print ("NUMERO PRIMO")
+                return True
---
+++ testprimos.py
@@ -0,0 +1,7 @@
+import pytest
+from main2 import funcion
+
+def test_answer ():
+    assert funcion(2) == True
+    
+test_answer()
